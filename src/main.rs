a8ab2eb39e19b9e4c5c948b55903cde976da5b5a
yamada-y0 <yama102130@gmail.com>
2024-11-04 16:54:43 +0900

    add: サンプルコードを追加

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    println!("{}", get_message());
+}
+
+fn get_message() -> &'static str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_message;
+
+    #[test]
+    fn test_get_message() {
+        assert_eq!(get_message(), "Hello, world!");
+    }
+}
